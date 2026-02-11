ea42dc33fd3f09b4631355ad2e106fbd8a8b39ef
hryui <158452005+hryui@users.noreply.github.com>
2026-02-11 22:47:51 +0800

    v2 js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 @ -0,0 +1,20 @@
 onload = () => {
-    document.body.classList.remove("container");
-  };
+    document.addEventListener("DOMContentLoaded", () => {
 
-  const wrapper = document.querySelector(".wrapper");
-  const openBtn = document.getElementById("openBtn");
-  const closeBtn = document.getElementById("closeBtn");
-  
-  openBtn.addEventListener("click", () => {
-      wrapper.classList.add("open");
-      openBtn.style.display = "none";
-      closeBtn.style.display = "inline-block";
-  });
-  
-  closeBtn.addEventListener("click", () => {
-      wrapper.classList.remove("open");
-      closeBtn.style.display = "none";
-      openBtn.style.display = "inline-block";
-  });
+    const wrapper = document.querySelector(".wrapper");
+    const openBtn = document.getElementById("openBtn");
+    const closeBtn = document.getElementById("closeBtn");
+
+    if (!wrapper || !openBtn || !closeBtn) {
+        console.error("One or more elements not found.");
+        return;
+    }
+
+    openBtn.addEventListener("click", () => {
+        wrapper.classList.add("open");
+        openBtn.style.display = "none";
+        closeBtn.style.display = "inline-block";
+    });
+
+    closeBtn.addEventListener("click", () => {
+        wrapper.classList.remove("open");
+        closeBtn.style.display = "none";
+        openBtn.style.display = "inline-block";
+    });
+
+});
+
   
